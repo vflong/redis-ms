efac8e7ed5fdce01c4a94514a0ff967287124954
weifeilong <weifeilong2013@gmail.com>
2018-07-04 15:54:10 +0800

    fix svc name change

--- redis-slave/run.sh
+++ redis-slave/run.sh
@@ -17,5 +17,5 @@
 if [[ ${GET_HOSTS_FROM:-dns} == "env" ]]; then
   redis-server --slaveof ${REDIS_MASTER_SERVICE_HOST} 6379
 else
-  redis-server --slaveof redis-master 6379
+  redis-server --slaveof lifesense-qa2-redis-master 6379
 fi
